27e66e16d6a68d4a9cac3d624bef4278b382eb22
ababababw <ng.minhluan.05072012@gmail.com>
2026-05-30 08:16:12 +0700

    Add files via upload

--- main10.py
+++ main10.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-st.image(r"c:\Users\Admin\Downloads\Gemini_Generated_Image_gasiu5gasiu5gasi.png")
-
 appetizer = {
     'Khoai tây chiên (20k)': 20,
     'Salad gà (25k)': 25,
