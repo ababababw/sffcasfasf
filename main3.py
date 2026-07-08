d06b6bdb2039c7e98402ba3b8af13e8c592226be
ababababw <ng.minhluan.05072012@gmail.com>
2026-07-08 14:59:43 +0700

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Biểu đồ phổ điểm thi lớp 10")
+
+df = pd.read_csv("D:\pro 5\diemthi10.csv")
+
+
+st.write("Dữ liệu:")
+st.dataframe(df)
+
+
+st.write("Biểu đồ môn Toán")
+st.bar_chart(df, x= "diem",y = "Mon Toan (So HS)" )
+
+st.write("Biểu đồ môn Văn")
+st.bar_chart(df, x= "diem",y = "Mon Ngu Van (So HS)" )
+
+st.write("Biểu đồ môn Anh")
+st.bar_chart(df, x= "diem",y = "Mon Tieng Anh (So HS)")
