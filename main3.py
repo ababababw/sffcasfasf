cca13417d85cfa51a7998201ab87b6850a97f490
ababababw <ng.minhluan.05072012@gmail.com>
2026-07-08 15:03:30 +0700

    Add files via upload

--- main3.py
+++ main3.py
@@ -3,18 +3,16 @@ import pandas as pd
 
 st.title("Biểu đồ phổ điểm thi lớp 10")
 
-df = pd.read_csv("D:\pro 5\diemthi10.csv")
-
+df = pd.read_csv(r"D:\pro 5\diemthi10.csv")
 
 st.write("Dữ liệu:")
 st.dataframe(df)
 
-
 st.write("Biểu đồ môn Toán")
-st.bar_chart(df, x= "diem",y = "Mon Toan (So HS)" )
+st.bar_chart(df, x="diem", y="Mon Toan (So HS)")
 
 st.write("Biểu đồ môn Văn")
-st.bar_chart(df, x= "diem",y = "Mon Ngu Van (So HS)" )
+st.bar_chart(df, x="diem", y="Mon Ngu Van (So HS)")
 
 st.write("Biểu đồ môn Anh")
-st.bar_chart(df, x= "diem",y = "Mon Tieng Anh (So HS)")
+st.bar_chart(df, x="diem", y="Mon Tieng Anh (So HS)")
