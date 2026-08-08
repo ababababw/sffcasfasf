9bb0fade84f222fc1abe00fcaf943f914ddcd099
ababababw <ng.minhluan.05072012@gmail.com>
2026-08-08 16:16:21 +0700

    Add files via upload

---
+++ main7.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_csv("D:\\pro 5\\data\\data5.8.csv")
+
+so_gio_hoc = df["Số Giờ Học"]
+
+itnhat = so_gio_hoc[0]
+for i in so_gio_hoc:
+    if i < itnhat:
+        itnhat = i
+
+nhieunhat = so_gio_hoc[0]
+for i in so_gio_hoc:
+    if i > nhieunhat:
+        nhieunhat = i
+
+tb = sum(so_gio_hoc) / len(so_gio_hoc)
+trungvi = so_gio_hoc.median()
+xuathiennhieunhat = so_gio_hoc.mode()
+
+st.write("giá trị nhỏ nhất:", itnhat)
+st.write("giá trị nhiều nhất:", nhieunhat)
+st.write("trung binhg:", tb)
+st.write("trung vị:", trungvi)
+st.write("xuất hiện nhiều nhất:", xuathiennhieunhat)
+
+gioi = df[df["Điểm Số"] >= 80]
+
+diachi = gioi["Địa Chỉ"]
+
+st.write("\nThành phố có nhiều điểm giỏi nhất:")
+st.write(diachi.mode())
